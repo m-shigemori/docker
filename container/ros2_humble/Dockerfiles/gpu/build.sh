ff3ab704a4934d0d2aac4917950f8c368c3abf24
m-shigemori <yasumasashige790@gmail.com>
2025-07-13 00:56:17 +0900

    [FIX] Docker ver

--- container/ros2_humble/Dockerfiles/gpu/build.sh
+++ container/ros2_humble/Dockerfiles/gpu/build.sh
@@ -4,20 +4,22 @@ DIR=$(pwd)
 str=`echo ${DIR} | awk -F "/" '{ print $(NF - 2) }'`
 
 echo "CUDAバージョンを選択してください:"
-echo "1. CUDA 12.2.2 (cuDNN 8.9)"
-echo "2. CUDA 12.5.1 (cuDNN 9.2)"
-echo "3. CUDA 12.6.3 (cuDNN 9.5)"
-read -p "選択肢の番号を入力してください (1-3): " choice
+echo "1. CUDA 12.2.2 (cuDNN 8.x, devel, Ubuntu 22.04)"
+echo "2. CUDA 12.5.1 (cuDNN, devel, Ubuntu 22.04)"
+echo "3. CUDA 12.8.1 (cuDNN, devel, Ubuntu 22.04)"
+echo "4. CUDA 12.9.1 (cuDNN, devel, Ubuntu 22.04)"
+read -p "選択肢の番号を入力してください (1-4): " choice
 
 CUDA_VERSION_TAG=""
 case $choice in
     1) CUDA_VERSION_TAG="12.2.2-cudnn8" ;;
-    2) CUDA_VERSION_TAG="12.5.1-cudnn9" ;;
-    3) CUDA_VERSION_TAG="12.6.3-cudnn9" ;;
+    2) CUDA_VERSION_TAG="12.5.1-cudnn" ;;
+    3) CUDA_VERSION_TAG="12.8.1-cudnn" ;;
+    4) CUDA_VERSION_TAG="12.9.1-cudnn" ;;
     *) echo "無効な選択です。終了します。" ; exit 1 ;;
 esac
 
-echo "選択されたCUDAバージョン: ${CUDA_VERSION_TAG}"
+echo "選択されたCUDAバージョン: ${CUDA_VERSION_TAG}-devel-ubuntu22.04"
 
 docker build \
     --tag sobits/${str} \
