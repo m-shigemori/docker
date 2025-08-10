ae2f725f27c1577fa9fc9edb917fccab2f76e296
m-shigemori <yasumasashige790@gmail.com>
2025-08-11 00:17:42 +0900

    [FIX]

--- setup_sh/install_docker.sh
+++ setup_sh/install_docker.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-set -e
 
-echo "パッケージリストを更新..."
 sudo apt-get update -y
 sudo apt-get install -y ca-certificates curl
+sudo apt install python3-pip -y
 
-echo "DockerのGPG鍵とリポジトリを追加..."
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -15,20 +13,38 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-echo "Docker Engineと関連ツールをインストール..."
 sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-echo "現在のユーザーをdockerグループに追加..."
 sudo usermod -aG docker $USER
-newgrp docker
 
-echo "Dockerサービスを有効化して起動..."
 sudo systemctl enable docker
 sudo systemctl start docker
 
-echo "PyQt5をインストール..."
-pip3 install PyQt5 --break-system-packages
-echo "alias ce='python3 docker/scripts/main.py'" >> ~/.bashrc
+newgrp docker
 
-echo "DockerのインストールとPyQt5の設定が完了
+pip3 install PyQt5 --break-system-packages
+
+cat << 'EOF' >> ~/.bashrc
+
+dockerpid="$(docker ps -q | head -1)"
+function dock() {
+  if [ "$1" == "-h" ]; then
+    echo "Attaches this window as a new terminal (bash) instance to a running docker container"
+    echo "Usage: 'dock' or 'dock {container_id_or_name}'"
+    echo "If no ID is given, then attaches to first found process."
+  elif [ $# -eq 0 ]; then
+    echo "Running Docker Container: $dockerpid"
+    docker exec -it "$dockerpid" /bin/bash
+  elif [ "$1" != "-h" ]; then
+    docker exec -it "$1" /bin/bash
+  fi
+}
+if [[ "$dockerpid" != "" ]]; then
+    dock
+else
+    echo "No docker container is running"
+fi
+
+alias ce='python3 ~/docker/scripts/main.py'
+EOF
--- setup_sh/install_nvidia_docker.sh
+++ setup_sh/install_nvidia_docker.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
-set -e
 
-echo "Dockerをインストール..."
-./install_docker.sh
-
-echo "NVIDIA Container ToolkitのリポジトリとGPG鍵を追加..."
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
   && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
-  sed 's#deb https://#deb [signed-by=/usr/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-  sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
-echo "NVIDIA Container Toolkitをインストール..."
-sudo apt-get update -y
+sudo apt-get update
 sudo apt-get install -y nvidia-container-toolkit
 
-echo "DockerとNVIDIA Container Toolkitを統合..."
 sudo nvidia-ctk runtime configure --runtime=docker
 
-echo "コンテナ設定ファイルを編集..."
-sudo sed -i -e 's/^#no-cgroups = .*/no-cgroups = false/' -e 's/^no-cgroups = .*/no-cgroups = false/' /etc/nvidia-container-runtime/config.toml
-
-echo "Dockerサービスを再起動..."
 sudo systemctl restart docker
-
-echo "NVIDIA Dockerのセットアップが完了"
