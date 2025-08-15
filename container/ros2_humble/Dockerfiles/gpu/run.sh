42b815c36f4e2240e29d51a87a8545251965e47f
m-shigemori <yasumasashige790@gmail.com>
2025-08-15 19:09:34 +0900

    [UPD]

--- container/ros2_humble/Dockerfiles/gpu/run.sh
+++ container/ros2_humble/Dockerfiles/gpu/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 DIR=$(pwd)
-str=`echo ${DIR} | awk -F "/" '{ print $(NF - 2) }'`
+str=$(echo "${DIR}" | awk -F "/" '{ print $(NF - 2) }')
 
-# 実行するイメージ名を sobits/${str} に固定 (build.shで設定されたタグと一致)
+# 実行するイメージ名を sobits/${str} に固定
 IMAGE_NAME="sobits/${str}"
 
-cd $(pwd)/../../
+cd "$(pwd)/../../"
 
 if [ ! -d "src" ]; then
     mkdir -p src
@@ -22,11 +22,12 @@ docker run -it \
     --env PULSE_SERVER=unix:/run/user/$(id -u)/pulse/native \
     --volume /run/user/$(id -u)/pulse:/run/user/$(id -u)/pulse \
     --volume /etc/udev/rules.d/:/etc/udev/rules.d/ \
-    --volume $(pwd)/src/:/home/sobits/colcon_ws/src/ \
+    --volume /dev/bus/usb:/dev/bus/usb \
+    --volume "$(pwd)/src/":/home/sobits/colcon_ws/src/ \
     --shm-size=1g \
     --net host \
     --name ${str} \
     --privileged \
     --user $(id -u):$(id -g) \
-    sobits/${str} \
+    ${IMAGE_NAME} \
     /bin/bash
