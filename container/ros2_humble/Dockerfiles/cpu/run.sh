e6f7ef06d43920139e717bf33a7bffdc930e407f
m-shigemori <yasumasashige790@gmail.com>
2025-12-24 00:23:50 +0900

    [UPD]

--- container/ros2_humble/Dockerfiles/cpu/run.sh
+++ container/ros2_humble/Dockerfiles/cpu/run.sh
@@ -3,16 +3,39 @@
 DIR=$(pwd)
 STR=$(echo "${DIR}" | awk -F "/" '{ print $(NF - 2) }')
 IMAGE_NAME="sobits/${STR}"
+CONTAINER_NAME="${STR}"
 
 cd "$(pwd)/../../"
 
-mkdir -p src
+if [ ! -d "src" ]; then
+    mkdir -p src
+fi
+
 xhost +local:${USER}
+
+VIDEO_DEVICES=""
+for dev in /dev/video*; do
+    [ -e "$dev" ] && VIDEO_DEVICES="$VIDEO_DEVICES --device $dev:$dev"
+done
+
+SERIAL_DEVICES=""
+for dev in /dev/ttyUSB* /dev/ttyACM* /dev/ttyS*; do
+    [ -e "$dev" ] && SERIAL_DEVICES="$SERIAL_DEVICES --device $dev:$dev"
+done
 
 docker run -it \
     --device /dev/snd \
+    --device /dev/dri \
+    --device /dev/input \
+    $VIDEO_DEVICES \
+    $SERIAL_DEVICES \
     --group-add audio \
-    --env CONTAINER_NAME="${STR}" \
+    --group-add video \
+    --group-add dialout \
+    --group-add input \
+    --group-add render \
+    --group-add netdev \
+    --env CONTAINER_NAME="${CONTAINER_NAME}" \
     --env DISPLAY="${DISPLAY}" \
     --env PULSE_SERVER=unix:/run/user/$(id -u)/pulse/native \
     --volume /run/user/$(id -u)/pulse:/run/user/$(id -u)/pulse \
@@ -24,7 +47,7 @@ docker run -it \
     --net host \
     --privileged \
     --shm-size=1g \
-    --name "${STR}" \
+    --name "${CONTAINER_NAME}" \
     --user $(id -u):$(id -g) \
     "${IMAGE_NAME}" \
     /bin/bash
