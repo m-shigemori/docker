132ca817f5c9bbe7d7dc645d085d4411ec2d1dbd
m-shigemori <yasumasashige790@gmail.com>
2026-01-25 04:20:46 +0900

    [upd]

--- container/Dockerfiles/up.sh
+++ container/Dockerfiles/up.sh
@@ -5,11 +5,21 @@ xhost +local:$USER > /dev/null
 export SCRIPT_DIR=$(cd $(dirname "$0"); pwd)
 export PARENT_DIR_NAME=$(basename "$(dirname "$SCRIPT_DIR")")
 
+if [ -f "${SCRIPT_DIR}/.env" ]; then
+    export $(grep -v '^#' "${SCRIPT_DIR}/.env" | xargs)
+fi
+
 export LOCAL_UID=$(id -u)
 export LOCAL_GID=$(id -g)
 export ROS_DISTRO=${ROS_DISTRO:-humble}
 export IMAGE_NAME="sobits/${PARENT_DIR_NAME}"
 
+if [ "$USE_GPU" = "true" ]; then
+    export PROFILE="gpu"
+else
+    export PROFILE="cpu"
+fi
+
 mkdir -p "${SCRIPT_DIR}/../src"
 
-docker compose -p "${PARENT_DIR_NAME}" -f "${SCRIPT_DIR}/compose.yaml" up -d --build --remove-orphans
+docker compose -p "${PARENT_DIR_NAME}" -f "${SCRIPT_DIR}/compose.yaml" --profile "${PROFILE}" up -d --build --remove-orphans
