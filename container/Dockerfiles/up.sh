d5509dbda1c05bae49016f8801ee3d1f4ecd0b17
m-shigemori <yasumasashige790@gmail.com>
2026-03-04 22:26:10 +0900

    [UPD]

--- container/Dockerfiles/up.sh
+++ container/Dockerfiles/up.sh
@@ -7,13 +7,14 @@ if [ -f "${DIR}/.env" ]; then
     export $(grep -v '^#' "${DIR}/.env" | xargs)
 fi
 
+export USER_NAME=${USER_NAME:-sobits}
 export LOCAL_UID=$(id -u)
 export LOCAL_GID=$(id -g)
+export GITHUB_TOKEN=$(gh auth token)
+
 export ROS_DISTRO=${ROS_DISTRO:-humble}
-export USER_NAME=${USER_NAME:-sobits}
 export IMAGE_NAME="sobits/${NAME}"
 export PARENT_DIR_NAME="${NAME}"
-export GITHUB_TOKEN=$(gh auth token)
 
 [ "$USE_GPU" = "true" ] && PROFILE="gpu" || PROFILE="cpu"
 
