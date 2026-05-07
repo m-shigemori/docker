57d1a4bca0b8bc2d86fde2c1694f8ae4fcbf0172
m-shigemori <yasumasashige790@gmail.com>
2026-05-07 14:14:02 +0900

    [ADD] Create config for path management

---
+++ app/config.py
@@ -0,0 +1,7 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+ASSETS_DIR = os.path.join(BASE_DIR, "assets")
+IMAGES_DIR = os.path.join(ASSETS_DIR, "images")
+ICONS_DIR = os.path.join(ASSETS_DIR, "icons")
