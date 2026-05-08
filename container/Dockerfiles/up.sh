96124a7b1d17a87c485b353a70991c57c73df22c
m-shigemori <yasumasashige790@gmail.com>
2026-05-08 18:41:03 +0900

    [FIX] Quote variables and simplify env export in up script

--- container/Dockerfiles/up.sh
+++ container/Dockerfiles/up.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-DIR=$(cd $(dirname "$0"); pwd)
-NAME=$(basename "$(dirname "$DIR")")
+DIR="$(cd "$(dirname "$0")" && pwd)"
+NAME="$(basename "$(dirname "$DIR")")"
 
 SRC_DIR="${DIR}/../src"
 mkdir -p "${SRC_DIR}"
 
 [ ! -d "${SRC_DIR}/gemini_mcp_suite" ] && git clone -q https://github.com/m-shigemori/gemini_mcp_suite.git "${SRC_DIR}/gemini_mcp_suite"
 
-export $(grep -v '^#' "${DIR}/.env" | xargs)
+export $(cat "${DIR}/.env" | xargs)
 
-export NAME=${NAME}
+export NAME="${NAME}"
 export IMAGE_NAME="${USER_NAME}/${NAME}"
 
-export USER_ID=$(id -u)
-export GROUP_ID=$(id -g)
+export USER_ID="$(id -u)"
+export GROUP_ID="$(id -g)"
 
 docker compose -p "${NAME}" --profile "${DEVICE}" up -d --build --remove-orphans
